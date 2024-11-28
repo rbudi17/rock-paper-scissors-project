31349a6b14c62de6c2090daeb1dfc5caaa7f4891
rbudi17 <r.budiyanto17@gmail.com>
2024-11-28 19:51:26 +0700

    Build function getComputerChoice()

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("Linked console");
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  let computerChoice = "";
+
+  if (randomNumber <= 3) {
+    computerChoice = "ROCK";
+  } else if (randomNumber > 3 && randomNumber <= 7) {
+    computerChoice = "PAPER";
+  } else {
+    computerChoice = "SCISSORS";
+  }
+
+  return computerChoice;
+}
